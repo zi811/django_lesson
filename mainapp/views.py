0228655c4670ec711cf0479c75846f5489f738bc
zi811 <550x220@gmail.com>
2022-12-02 00:59:14 +0300

    lesson_2 Static site with Django

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,12 +1,47 @@
-from django.shortcuts import render
-from django.views.generic import View
-from django.http import HttpResponse
-# Create your views here.
+from datetime import datetime
 
-class HelloWorldView(View):
-    def get(self, *args, **kwargs):
-        return HttpResponse('hello world')
+from django.views.generic import TemplateView
 
 
-def check_kwargs(request, **kwargs):
-    return HttpResponse('kwatgs:<br>{kwargs}')
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        # Get all previous data
+        context = super().get_context_data(**kwargs)
+        # Create your own data
+        context["news_title"] = "Громкий новостной заголовок"
+        context[
+            "news_preview"
+        ] = "Предварительное описание, которое заинтересует каждого"
+        context["range"] = range(5)
+        context["datetime_obj"] = datetime.now()
+        return context
+
+class NewsWithPaginatorView(NewsPageView):
+
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
+        print(self.request.GET.get('param1'))
+        return context
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
