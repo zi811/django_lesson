690bc4eaaf3369459400f194cc0e14da4e574bd2
zi811 <550x220@gmail.com>
2022-12-01 20:51:14 +0300

    lesson_2 Hello world with cbv and FBV

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from django.http import HttpResponse
 # Create your views here.
+
+class HelloWorldView(View):
+    def get(self, *args, **kwargs):
+        return HttpResponse('hello world')
+
+
+def check_kwargs(request, **kwargs):
+    return HttpResponse('kwatgs:<br>{kwargs}')
